4c42be064f21d7d027d79ff0436360a38d7c94cf
Tejas2171 <tejassonawane11102004@gmail.com>
2025-03-06 10:24:29 +0530

    Message backend done except realtime socket programming

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 import { connectDB } from "./lib/db.js"
 
 dotenv.config()
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
     console.log(`server is listening on port ${PORT}`);
