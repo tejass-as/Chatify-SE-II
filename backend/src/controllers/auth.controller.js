b79697f408ae94315654d8f87acdfbe3a62229ea
Tejas2171 <tejassonawane11102004@gmail.com>
2025-03-06 09:41:37 +0530

    User backend done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs"
@@ -79,5 +80,32 @@ export const logout = (req, res) => {
     } catch (error) {
         console.log("Error in logout controller", error.message)
         res.status(500).json({message: "Internal server error"})
+    }
+}
+
+export const updateProfile = async (req, res) => {
+    try {
+        const {profilePic} = req.body;
+        const userID = req.user._id;
+
+        if(!profilePic)
+            return res.status(400).json({message:"Profile pic is not provided"})
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+        const updatedUser = await User.findByIdAndUpdate(userID, {profilePic: uploadResponse.secure_url}, {new:true})
+
+        res.status(200).json({message:"Profile pic updated"})
+    } catch (error) {
+        console.log("Error in update-profile controller")
+        res.status(500).json({message:"Internal server error"})
+    }
+}
+
+export const checkAuth = (req, res) => {
+    try {
+        res.status(200).json(req.user);
+    } catch(error) {
+        console.log("Error in checkAuth controller")
+        res.status(500).json({message:"Internal server error"})
     }
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 
 import authRoutes from "./routes/auth.route.js"
 import { connectDB } from "./lib/db.js"
@@ -9,7 +10,8 @@ const app = express();
 
 const PORT = process.env.PORT
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
